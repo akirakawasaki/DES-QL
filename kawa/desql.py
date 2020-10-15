14217bc2508d48c6d7e5ec2868430a63caea1024
kawasaki <kawasakiakira@Kawasaki-MacBook-Pro.local>
2020-10-15 16:25:09 +0900

    temperature conversion

--- kawa/desql.py
+++ kawa/desql.py
@@ -1,6 +1,7 @@
 # standard libraries
-import socket 
 import decimal
+import math
+import socket
 
 # third-party libraries
 import numpy as np
@@ -23,7 +24,140 @@ class tlm():
     #BUFSIZE = 1088
     #BUFSIZE = 1280
     #BUFSIZE = 2176
+
+    '''
+    Convert thermoelectric voltage (in uV) to temperature (in K)
+    '''
+    def uv2k(self, val, type):
+        if type != 'K': print('ERROR!')
+
+        # Ref.: NIST Monograph 175
+        if val < 0.0:
+            c0 = 0.0
+            c1 = 2.5173462e-2
+            c2 = -1.1662878e-6
+            c3 = -1.0833638e-9
+            c4 = -8.9773540e-13
+            c5 = -3.7342377e-16
+            c6 = -8.6632643e-20
+            c7 = -1.0450598e-23
+            c8 = -5.1920577e-28
+            c9 = 0.0
+        elif val < 20644.0:
+            c0 = 0.0
+            c1 = 2.508355e-2
+            c2 = 7.860106e-8
+            c3 = -2.503131e-10
+            c4 = 8.315270e-14
+            c5 = -1.228034e-17
+            c6 = 9.804036e-22
+            c7 = -4.413030e-26
+            c8 = 1.057734e-30
+            c9 = -1.052755e-35
+        else:
+            c0 = -1.318058e2
+            c1 = 4.830222e-2
+            c2 = -1.646031e-6
+            c3 = 5.464731e-11
+            c4 = -9.650715e-16
+            c5 = 8.802193e-21
+            c6 = -3.110810e-26
+            c7 = 0.0
+            c8 = 0.0
+            c9 = 0.0
+
+        y =  c0 \
+           + c1 * val \
+           + c2 * val**2 \
+           + c3 * val**3 \
+           + c4 * val**4 \
+           + c5 * val**5 \
+           + c6 * val**6 \
+           + c7 * val**7 \
+           + c8 * val**8 \
+           + c9 * val**9 \
+           + 273.15         # convert deg-C to Kelvin
+
+        return y
+
+    '''
+    Convert temperature (in K) to thermoelectric voltage (in uV)
+    '''
+    def k2uv(self, val, type):
+        if type != 'K': print('ERROR!')
+
+        val2 = val - 273.15     # convert Kelvin to deg-C
+
+        # Ref.: NIST Monograph 175
+        if val2 < 0.0:
+            c0 = 0.0
+            c1 = 3.9450128025e1
+            c2 = 2.3622373598e-2
+            c3 = -3.2858906784e-4
+            c4 = -4.9904828777e-6
+            c5 = -6.7509059173e-8
+            c6 = -5.7410327428e-10
+            c7 = -3.1088872894e-12
+            c8 = -1.0451609365e-14
+            c9 = -1.9889266878e-17
+            c10 = -1.6322697486e-20
+            alp0 = 0.0
+            alp1 = 0.0
+        else:
+            c0 = -1.7600413686e1
+            c1 = 3.8921204975e1
+            c2 = 1.8558770032e-2
+            c3 = -9.9457592874e-5
+            c4 = 3.1840945719e-7
+            c5 = -5.6072844889e-10
+            c6 = 5.6075059059e-13
+            c7 = -3.2020720003e-16
+            c8 = 9.7151147152e-20
+            c9 = -1.2104721275e-23
+            c10 = 0.0
+            alp0 = 1.185976e2
+            alp1 = -1.183432e-4
+
+        y =  c0 \
+            + c1 * val2 \
+            + c2 * val2**2 \
+            + c3 * val2**3 \
+            + c4 * val2**4 \
+            + c5 * val2**5 \
+            + c6 * val2**6 \
+            + c7 * val2**7 \
+            + c8 * val2**8 \
+            + c9 * val2**9 \
+            + c10 * val2**10 \
+            + alp0 * math.exp(alp1 * (val2 - 126.9686)**2)
+        
+        return y
+
+    '''
+    Convert thermistor voltage to resistance
+    '''
+    def v2ohm(self, val):
+        # Ref.: Converting NI 9213 Data (FPGA Interface)
+        return (1.0e4 * 32 * val) / (2.5 - 32 * val)
+
+    '''
+    Convert thermistor resistance to temperature (in K)
+    '''
+    def ohm2k(self, val):
+        if val > 0:
+            # Ref.: Converting NI 9213 Data (FPGA Interface)
+            a = 1.2873851e-3
+            b = 2.3575235e-4
+            c = 9.4978060e-8
+            y = 1.0 / (a + b * math.log(val) + c * (math.log(val)**3)) - 1.0
+        else:
+            y = 273.15
+        
+        return y
     
+    '''
+    Constractor
+    '''
     def __init__(self, tlm_type):
         #self.HOST = socket.gethostname()
         self.HOST = ''
@@ -63,6 +197,9 @@ class tlm():
         # initialize data index
         self.iData = 0
 
+    '''
+    Destractor
+    '''
     def __del__(self):
         self.udpSoc.close()
 
@@ -80,19 +217,23 @@ class tlm():
         # sweep frames in a major frame
         for iFrame in range(self.NUM_OF_FRAMES):
             #print(f"iData: {self.iData}")
-            
-            # initialize the row by filling wit NaN
-            self.df.loc[self.iData] = np.nan
 
             adrs_tmp = iFrame * self.W2B * (self.LEN_HEADER + self.LEN_PAYLOAD)
-            #print(f"adrs_tmp: {adrs_tmp}")
+            #print(f"adrs_tmp: {adrs_tmp}") 
+
+            # initialize the row by filling wit NaN
+            self.df.loc[self.iData] = np.nan
             
-            # GSE days from January 1st 
+            # pick up data from the datagram
+            '''
+            When w assgn < 0
+            '''
+            # Days from January 1st on GSE
             adrs = adrs_tmp + self.W2B * 0
             self.df.iat[self.iData,0] =  (self.data[adrs]   >> 4  ) * 100 \
                                        + (self.data[adrs]   & 0x0F) * 10  \
                                        + (self.data[adrs+1] >> 4  ) * 1
-    
+        
             # GSE timestamp in [sec]
             adrs = adrs_tmp + self.W2B * 0
             self.df.iat[self.iData,1] =  (self.data[adrs+1] & 0x0F) * 10  * 3600 \
@@ -104,115 +245,100 @@ class tlm():
                                        + (self.data[adrs+4] & 0x0F) * 100 / 1000 \
                                        + (self.data[adrs+5] >> 4  ) * 10  / 1000 \
                                        + (self.data[adrs+5] & 0x0F) * 1   / 1000 
-    
-            # frame counter generated by the telemeter
-            #adrs = adrs_tmp + self.W2B * (self.LEN_HEADER + 32)
-            #self.df.iat[self.iData,2] =  self.data[adrs]   * 2**8 \
-            #                           + self.data[adrs+1]
-            
-            # pick up data from the datagram
+
+            '''
+            When w assgn >= 0
+            '''   
             for iItem in range(2, self.NUM_OF_ITEMS):
-                if self.df_cfg.at[iItem,'w assgn'] < 0: continue    # skip
-                
                 # designate byte addres with the datagram
                 adrs = adrs_tmp + self.W2B * (self.LEN_HEADER + self.df_cfg.at[iItem,'w assgn'])
 
                 # frame/loop counter
                 if self.df_cfg.at[iItem,'type'] == 'counter':
-                    #self.df.iat[self.iData,iItem] = int.from_bytes(self.data[adrs], self.data[adrs+1], byteorder='big', signed=False)
-                    self.df.iat[self.iData,iItem] =  self.data[adrs]   * 2**8 \
-                                                   + self.data[adrs+1]
+                    self.df.iat[self.iData,iItem] = \
+                        int.from_bytes((self.data[adrs], self.data[adrs+1]), byteorder='big', signed=False)
+                    #self.df.iat[self.iData,iItem] =  self.data[adrs]   * 2**8 \
+                    #                               + self.data[adrs+1]
                 
                 # DES timestamp in [sec]
                 elif self.df_cfg.at[iItem,'type'] == 'des time':
-                    #self.df.iat[self.iData,iItem] = \
-                    #    int.from_bytes((self.data[adrs],self.data[adrs+1],self.data[adrs+2],self.data[adrs+3]), byteorder='big', signed=False)
-                    self.df.iat[self.iData,iItem] = (  self.data[adrs]   * 2**(24) \
-                                                     + self.data[adrs+1] * 2**(16) \
-                                                     + self.data[adrs+2] * 2**(8)  \
-                                                     + self.data[adrs+3] * 2**(0)  ) / 1000.0
+                    self.df.iat[self.iData,iItem] = \
+                        int.from_bytes((self.data[adrs], self.data[adrs+1], self.data[adrs+2], self.data[adrs+3]), 
+                                        byteorder='big', signed=False) \
+                            / 1000.0
+                    #self.df.iat[self.iData,iItem] = (  self.data[adrs]   * 2**(24) \
+                    #                                 + self.data[adrs+1] * 2**(16) \
+                    #                                 + self.data[adrs+2] * 2**(8)  \
+                    #                                 + self.data[adrs+3] * 2**(0)  ) / 1000.0
 
                 # pressure in [MPa]
                 elif self.df_cfg.at[iItem,'type'] == 'p':
-                    self.df.iat[self.iData,iItem] =  \
-                          self.df_cfg.at[iItem,'coeff a'] / float(2**11) \
+                    self.df.iat[self.iData,iItem] = \
+                          self.df_cfg.at[iItem,'coeff a'] / 2**11 \
                             * int.from_bytes((self.data[adrs], self.data[adrs+1]), byteorder='big', signed=True) \
                         + self.df_cfg.at[iItem,'coeff b']
-
-                    #self.df.iat[self.iData,iItem] =  \
-                    #      float(self.df_cfg.at[iItem,'coeff a']) \
-                    #        * float((self.data[adrs] & 0x7F) * 2**8 + self.data[adrs+1]) / float(2**10) \
-                    #    + float(self.df_cfg.at[iItem,'coeff b'])
-                    
-                    #dTmp =  (-(self.data[adrs] & 0b10000000) | (self.data[adrs]   & 0b01111111) ) * 2**8 \
-                    #      + (                                   self.data[adrs+1] & 0b11111111  )
-                    #self.df.iat[self.iData,iItem] =  \
-                    #      float(self.df_cfg.at[iItem,'coeff a']) * float(dTmp) / float(2**15) * 10.0 \
-                    #    + float(self.df_cfg.at[iItem,'coeff b'])
                                     
-                # temperature in [uV]
+                # temperature in [K]
                 elif self.df_cfg.at[iItem,'type'] == 'T':
-                    self.df.iat[self.iData,iItem] = \
-                          self.df_cfg.at[iItem,'coeff a'] \
+                    # TC thermoelectric voltage in [uV]
+                    Vtc =  self.df_cfg.at[iItem,'coeff a'] / 2**18 * 1e6 \
                             * int.from_bytes((self.data[adrs], self.data[adrs+1]), byteorder='big', signed=True) \
-                        + self.df_cfg.at[iItem,'coeff b']
+                         + self.df_cfg.at[iItem,'coeff b']
+                    Ttc = self.uv2k(Vtc + Vcjc - Vaz, 'K')
 
-                    #self.df.iat[self.iData,iItem] = \
-                    #      self.df_cfg.at[iItem,'coeff a'] / float(2**18) * 1000000.0 \
-                    #        * int.from_bytes((self.data[adrs], self.data[adrs+1]), byteorder='big', signed=True) \
-                    #    + self.df_cfg.at[iItem,'coeff b']
+                    self.df.iat[self.iData,iItem] = Ttc
 
-                    #self.df.iat[self.iData,iItem] = \
-                    #      self.df_cfg.at[iItem,'coeff a'] / 2.0**(13) \
-                    #        * int.from_bytes(self.data[adrs:adrs+1], byteorder='big', signed=True) \
-                    #    + self.df_cfg.at[iItem,'coeff b']
+                    #print(f"Vtc : {Vtc}")
+                    #print(f"Vcjc: {Vcjc}")
+                    #print(f"Vaz : {Vaz}")
 
-                    #self.df.iat[self.iData,iItem] =  \
-                    #      float(self.df_cfg.at[iItem,'coeff a']) \
-                    #        * float((self.data[adrs] & 0x7F) * 2**8 + self.data[adrs+1]) / float(2**13) \
-                    #    + float(self.df_cfg.at[iItem,'coeff b'])
-
-                # auto zero in [uV]
+                # auto-zero coefficient in [uV]
                 elif self.df_cfg.at[iItem,'type'] == 'az':
-                    self.df.iat[self.iData,iItem] = \
-                          self.df_cfg.at[iItem,'coeff a'] \
+                    Vaz =  self.df_cfg.at[iItem,'coeff a'] / 2**18 * 1e6 \
                             * int.from_bytes((self.data[adrs], self.data[adrs+1]), byteorder='big', signed=True) \
-                        + self.df_cfg.at[iItem,'coeff b']
+                         + self.df_cfg.at[iItem,'coeff b']
+                    
+                    self.df.iat[self.iData,iItem] = Vaz
 
-                    #self.df.iat[self.iData,iItem] = \
-                    #      self.df_cfg.at[iItem,'coeff a'] / float(2**18) * 1000000.0  \
-                    #        * int.from_bytes((self.data[adrs], self.data[adrs+1]), byteorder='big', signed=True) \
-                    #    + self.df_cfg.at[iItem,'coeff b']
-
-                # cold-junction compensation (binary)
+                # cold-junction compensation coefficient in [uV]
                 elif self.df_cfg.at[iItem,'type'] == 'cjc':
-                    self.df.iat[self.iData,iItem] = \
-                          self.df_cfg.at[iItem,'coeff a'] \
+                    cjc =  self.df_cfg.at[iItem,'coeff a'] / 2**18 \
                             * int.from_bytes((self.data[adrs], self.data[adrs+1]), byteorder='big', signed=True) \
-                        + self.df_cfg.at[iItem,'coeff b']
+                         + self.df_cfg.at[iItem,'coeff b']
+                    Rcjc = self.v2ohm(cjc)
+                    Tcjc = self.ohm2k(Rcjc)
+                    Vcjc = self.k2uv(Tcjc, 'K')
+
+                    #print(f"CJC : {cjc}")
+                    #print(f"Rcjc: {Rcjc}")
+                    #print(f"Tcjc: {Tcjc}")
+                    #print(f"Vcjc: {Vcjc}")
+
+                    self.df.iat[self.iData,iItem] = Vcjc
 
                 # analog pressure in [MPa]
                 elif self.df_cfg.at[iItem,'type'] == 'p ana':
                     if iFrame % self.df_cfg.at[iItem,'sub com mod'] != self.df_cfg.at[iItem,'sub com res']: continue
                     
-                    self.df.iat[self.iData,iItem] =  \
-                          self.df_cfg.at[iItem,'coeff a'] * 5.0 / float(2**16) \
+                    self.df.iat[self.iData,iItem] = \
+                          self.df_cfg.at[iItem,'coeff a'] / 2**16 * 5.0 \
                             * int.from_bytes((self.data[adrs],self.data[adrs+1]), byteorder='big', signed=True) \
                         + self.df_cfg.at[iItem,'coeff b']
 
                 # voltage in [V]
                 elif self.df_cfg.at[iItem,'type'] == 'V':
-                    self.df.iat[self.iData,iItem] =  \
+                    self.df.iat[self.iData,iItem] = \
                           self.df_cfg.at[iItem,'coeff a'] \
                             * int.from_bytes((self.data[adrs],self.data[adrs+1]), byteorder='big', signed=True) \
                         + self.df_cfg.at[iItem,'coeff b']
 
                 # relay status (boolean)
                 elif self.df_cfg.at[iItem,'type'] == 'rel':
-                    #self.df.iat[self.iData,iItem] = int.from_bytes((self.data[adrs],self.data[adrs+1]), byteorder='big', signed=False)
                     self.df.iat[self.iData,iItem] = \
                         (self.data[adrs + self.df_cfg.at[iItem,'coeff b']] & int(self.df_cfg.at[iItem,'coeff a'])) \
                             / self.df_cfg.at[iItem,'coeff a']
+                    #self.df.iat[self.iData,iItem] = \
+                    #   int.from_bytes((self.data[adrs],self.data[adrs+1]), byteorder='big', signed=False)
 
                 # others
                 else:
@@ -235,8 +361,8 @@ smt = tlm('smt')
 
 NNN = 0
 #while NNN < 1:
-#while NNN <= 500:
-while NNN <= 30000:
+while NNN <= 500:
+#while NNN <= 10000:
     ### STEP 1: data receive
     smt.receive()
 
@@ -297,6 +423,17 @@ while NNN <= 30000:
 
     NNN += 1
 
+# for debug
+'''
+print(smt.uv2k(-1.0,'K')+273.15)
+print(smt.uv2k(1.0,'K')+273.15)
+print(smt.uv2k(20643.0,'K')+273.15)
+print(smt.uv2k(20645.0,'K')+273.15)
+print('')
+print(smt.k2uv(-1.0-273.15,'K'))
+print(smt.k2uv(1.0-273.15,'K'))
+print(smt.k2uv(500.0-273.15,'K'))
+'''
 
 ### STEP F: finalize
 del smt
