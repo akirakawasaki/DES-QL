17dc437a3543d346b8a11247bc4e9ea2172a2a5f
kawasaki <kawasaki@nuae.nagoya-u.ac.jp>
2021-01-24 19:49:21 +0900

    tan tative commit

--- kawa/tests/mpltest.py
+++ kawa/tests/mpltest.py
@@ -41,7 +41,7 @@ plt.rcParams["figure.subplot.hspace"] = 0.30    # Height Margin between subplots
 #
 
 # served as time in [s]
-x_val = list(map(lambda y: y/10.0, range(10000)))
+x_val = list(map(lambda y: y/1.0, range(10000)))
 
 # pseudo-data (series 1: sinusoidal)
 y1_val = list(map(lambda y: 2.0 * math.sin(y/5.0) + 2.0, x_val))    # 0 < y < 4
@@ -93,8 +93,8 @@ class frmMain(wx.Frame):
 #
 class MainPanel(wx.Panel):
     __T_RANGE = 30      # [s]
-    __N_PLOTTER = 1
-    #__N_PLOTTER = 2   
+    # __N_PLOTTER = 1
+    __N_PLOTTER = 2   
 
     # index_x = 1    
     # sensor_type = ['Time [s]', 'P [MPa]', 'T [K]', 'IMU', 'House Keeping']
@@ -131,10 +131,12 @@ class MainPanel(wx.Panel):
 
         # update plot points by appending latest values
         self.x_series = np.append(self.x_series, x_val[self.NNN])
-        self.y1_series = np.append(self.y1_series, y1_val[self.NNN])
-        self.y2_series = np.append(self.y2_series, y2_val[self.NNN])
+        # self.y1_series = np.append(self.y1_series, y1_val[self.NNN])
+        # self.y2_series = np.append(self.y2_series, y2_val[self.NNN])
+        for i in range(self.__N_PLOTTER):
+            self.y_series = np.append(self.y_series, self.y_val[i][self.NNN])
 
-        # pseudo-time incriment NEEDED ONLY FOR TEST
+        # pseudo-time incriment USED ONLY IN TEST
         self.NNN += 1
         if self.NNN == len(x_val):  self.NNN = 0   # reset pseudo-time
 
@@ -147,28 +149,38 @@ class MainPanel(wx.Panel):
         while self.x_series[0] < t_min:
             print("GUI PLT: a member of 'x_series' is out of the range")
             self.x_series = np.delete(self.x_series, 0)
-            self.y1_series = np.delete(self.y1_series, 0)
-            self.y2_series = np.delete(self.y2_series, 0)
+            self.y_series = np.delete(self.y_series, np.s_[0:self.__N_PLOTTER])
+            # self.y1_series = np.delete(self.y1_series, 0)
+            # self.y2_series = np.delete(self.y2_series, 0)
             # self.y_series = np.delete(self.y_series, self.__N_PLOT)
 
-        # delete x axis and lines by restroring canvas
-        self.canvas.restore_region(self.backgrounds[0])
+            print("GUI PLT: length x: {}, y: {}".format(len(self.x_series),len(self.y_series)))
 
-        # clear axes
-        self.axes[0].cla()
+        for i in range(self.__N_PLOTTER):
+            # delete x axis and lines by restroring canvas
+            self.canvas.restore_region(self.backgrounds[i])
 
-        # update limit for x axis
-        self.axes[0].set_xlim([t_min, t_max])
+        for i in range(self.__N_PLOTTER):
+            # clear axes
+            self.axes[i].cla()
 
-        # set limit for y axis
-        self.axes[0].set_ylim([self.y_min[0], self.y_max[0]])
+        for i in range(self.__N_PLOTTER):
+            # update limit for x axis
+            self.axes[i].set_xlim([t_min, t_max])
 
-        # set label for y axis
-        self.axes[0].set_ylabel(self.y_label[0])
+        for i in range(self.__N_PLOTTER):
+            # set limit for y axis
+            self.axes[i].set_ylim([self.y_min[i], self.y_max[i]])
 
-        # update alert line
-        self.axes[0].axhline(y=1.0, xmin=0, xmax=1, color='red')
-        # self.axes[1].axhline(y=500.0, xmin=0, xmax=1, color='red')
+        for i in range(self.__N_PLOTTER):
+            # set label for y axis
+            self.axes[i].set_ylabel(self.y_label[i])
+
+        for i in range(self.__N_PLOTTER):
+            # update alert line
+            self.axes[i].axhline(y=self.alart_lim_u[i], xmin=0, xmax=1, color='red')
+            self.axes[i].axhline(y=self.alart_lim_l[i], xmin=0, xmax=1, color='red')
+            # self.axes[1].axhline(y=500.0, xmin=0, xmax=1, color='red')
 
         # tentative draw
         #self.canvas.draw()
@@ -179,14 +191,18 @@ class MainPanel(wx.Panel):
         # update lines by prepared axes
         # NOTE: lines become iterrable hereafter
         self.lines = []
-        self.lines.append(self.axes[0].plot(self.x_series, self.y1_series)[0])
+        for i in range(self.__N_PLOTTER):
+            self.lines.append(self.axes[i].plot(self.x_series, self.y_series[i::self.__N_PLOTTER])[0])
      
-        # reflect updates in lines
-        self.axes[0].draw_artist(self.lines[0])
+        for i in range(self.__N_PLOTTER):
+            # reflect updates in lines
+            self.axes[i].draw_artist(self.lines[i])
 
-        # redraw and show updated canvas
-        self.fig.canvas.blit(self.axes[0].bbox)
-        self.fig.canvas.flush_events()
+        for i in range(self.__N_PLOTTER):
+            # redraw and show updated canvas
+            self.fig.canvas.blit(self.axes[i].bbox)
+        
+        # self.fig.canvas.flush_events()
         print("GUI PLT: redraw plots...")
 
     # Load configurations from external files
@@ -194,6 +210,8 @@ class MainPanel(wx.Panel):
         self.y_label = ["Series 1", "Series 2"]
         self.y_min = [0.0, 0.0]
         self.y_max = [4.0, 3.0]
+        self.alart_lim_u = [3.0, 2.5]
+        self.alart_lim_l = [1.0, 0.5]
         
         # self.item_plot = [self.df_cfg_plot['item'][self.df_cfg_plot['plot_1'].astype(bool)].iat[0],
         #                   self.df_cfg_plot['item'][self.df_cfg_plot['plot_2'].astype(bool)].iat[0]]
@@ -202,34 +220,40 @@ class MainPanel(wx.Panel):
     def configure_plotter(self):      
         # initialize data set
         self.x_series = np.empty(0)
-        # self.y_series = np.empty(0)
-        self.y1_series = np.empty(0)
-        self.y2_series = np.empty(0)
+        self.y_series = np.empty(0)
+        # self.y1_series = np.empty(0)
+        # self.y2_series = np.empty(0)
+        # print("GUI PLT: x_series = {}".format(self.x_series))
+        # print("GUI PLT: y_series = {}".format(self.y_series))
+
+        # USED ONLY IN TEST
+        self.y_val = [y1_val, y2_val]   
 
         # prepare empty matplotlib Fugure
         self.fig = Figure(figsize=(8, 8))
         
+        # register Figure with matplotlib Canvas
+        self.canvas = FigureCanvasWxAgg(self, wx.ID_ANY, self.fig)
+
+        # return None     # for debug
+
         ### prepare axes
         # - generate subplots containing axes in Figure
         # NOTE: axes become iterrable hereafter
         self.axes = []
-        self.axes.append(self.fig.add_subplot(1, 1, 1))
-        
-        # register Figure with matplotlib Canvas
-        self.canvas = FigureCanvasWxAgg(self, -1, self.fig)
+        for i in range(self.__N_PLOTTER):
+            self.axes.append(self.fig.add_subplot(self.__N_PLOTTER, 1, i+1))
 
-        # return None     # for debug
+            # - set limit for x axis
+            t_min = 0
+            self.axes[i].set_xlim([t_min, t_min + self.__T_RANGE])
 
-        # - set limit for y axis
-        self.axes[0].set_ylim([self.y_min[0], self.y_max[0]])
+            # - set limit for y axis
+            self.axes[i].set_ylim([self.y_min[i], self.y_max[i]])
 
-        # - set label for y axis
-        self.axes[0].set_ylabel(self.y_label[0])
+            # - set label for y axis
+            self.axes[i].set_ylabel(self.y_label[i])
 
-        # - set limit for x axis
-        # "DYNAMIC" X AXIS IS TO BE PREPARED UPON UPDATE ###
-        t_min = 0
-        self.axes[0].set_xlim([t_min, t_min + self.__T_RANGE])
 
         # tentatively draw canvas without plot points to save as background
         self.canvas.draw()                                            
@@ -237,7 +261,8 @@ class MainPanel(wx.Panel):
         # save the empty canvas as background
         # NOTE: backgrounds become iterrable hereafter
         self.backgrounds = []
-        self.backgrounds.append(self.canvas.copy_from_bbox(self.axes[0].bbox))
+        for i in range(self.__N_PLOTTER):
+            self.backgrounds.append(self.canvas.copy_from_bbox(self.axes[i].bbox))
 
 
 if __name__ =="__main__":
