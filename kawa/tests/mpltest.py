f9aee14638c2ce9aeed5a6d910c482c1fdd57a19
kawasaki <kawasaki@nuae.nagoya-u.ac.jp>
2021-01-25 00:34:58 +0900

    support scaling in number of plotters

--- kawa/tests/mpltest.py
+++ kawa/tests/mpltest.py
@@ -21,7 +21,7 @@ from matplotlib.figure import Figure
 
 
 ### wxPython configurations
-REFLESH_RATE_PLOTTER = 200
+REFLESH_RATE_PLOTTER = 500
 # REFLESH_RATE_PLOTTER = 1000
 # REFLESH_RATE_DIGITAL_INDICATOR = 450
 
@@ -44,12 +44,21 @@ plt.rcParams["figure.subplot.hspace"] = 0.30    # Height Margin between subplots
 x_val = list(map(lambda y: y/1.0, range(10000)))
 
 # pseudo-data (series 1: sinusoidal)
-y1_val = list(map(lambda y: 2.0 * math.sin(y/5.0) + 2.0, x_val))    # 0 < y < 4
+y1_val = list(map(lambda y: 2.0 * (math.sin(y/5.0) + 1.0), x_val))    # 0 < y < 4
 # xy1_val = list(zip(x_val, y1_val))
 
 # pseudo-data (series 2: random)
 y2_val = list(map(lambda y: 3.0 * random.random(), x_val))          # 0 < y < 3
 # xy2_val = list(zip(x_val, y2_val))
+
+# pseudo-data (series 3: random)
+y3_val = list(map(lambda y: 2.0 * (math.cos(2.0 * math.pi * random.random()) + 1.0), x_val))          # 0 < y < 4
+
+# pseudo-data (series 4: random)
+y4_val = list(map(lambda y: 1.0 * random.random(), x_val))          # 0 < y < 1
+
+# pseudo-data (series 5: random)
+y5_val = list(map(lambda y: 1.5 * random.random(), x_val))          # 0 < y < 1.5
 
 
 #
@@ -94,7 +103,7 @@ class frmMain(wx.Frame):
 class MainPanel(wx.Panel):
     __T_RANGE = 30      # [s]
     # __N_PLOTTER = 1
-    __N_PLOTTER = 2   
+    __N_PLOTTER = 5
 
     # index_x = 1    
     # sensor_type = ['Time [s]', 'P [MPa]', 'T [K]', 'IMU', 'House Keeping']
@@ -143,7 +152,7 @@ class MainPanel(wx.Panel):
         # update time range to plot
         t_max = self.x_series[-1]
         t_min = t_max - self.__T_RANGE
-        # print("t_max = {}, t_min = {}".format(t_max, t_min))
+        # print("GUI PLT: t_max = {}, t_min = {}".format(t_max, t_min))
 
         # delete plot points out of the designated time range
         while self.x_series[0] < t_min:
@@ -154,64 +163,106 @@ class MainPanel(wx.Panel):
             # self.y2_series = np.delete(self.y2_series, 0)
             # self.y_series = np.delete(self.y_series, self.__N_PLOT)
 
-            print("GUI PLT: length x: {}, y: {}".format(len(self.x_series),len(self.y_series)))
+            # print("GUI PLT: length x: {}, y: {}".format(len(self.x_series),len(self.y_series)))
 
+        # prepare redraw
+        self.lines = []
         for i in range(self.__N_PLOTTER):
             # delete x axis and lines by restroring canvas
             self.canvas.restore_region(self.backgrounds[i])
 
-        for i in range(self.__N_PLOTTER):
             # clear axes
             self.axes[i].cla()
 
-        for i in range(self.__N_PLOTTER):
             # update limit for x axis
             self.axes[i].set_xlim([t_min, t_max])
 
-        for i in range(self.__N_PLOTTER):
             # set limit for y axis
-            self.axes[i].set_ylim([self.y_min[i], self.y_max[i]])
+            self.axes[i].set_ylim([self.plt_attr[i].y_min, self.plt_attr[i].y_max])
 
-        for i in range(self.__N_PLOTTER):
             # set label for y axis
-            self.axes[i].set_ylabel(self.y_label[i])
+            self.axes[i].set_ylabel(self.plt_attr[i].y_label)
 
-        for i in range(self.__N_PLOTTER):
             # update alert line
-            self.axes[i].axhline(y=self.alart_lim_u[i], xmin=0, xmax=1, color='red')
-            self.axes[i].axhline(y=self.alart_lim_l[i], xmin=0, xmax=1, color='red')
-            # self.axes[1].axhline(y=500.0, xmin=0, xmax=1, color='red')
-
-        # tentative draw
-        #self.canvas.draw()
-
-        # save the empty canvas as background
-        #self.backgrounds[0] = self.canvas.copy_from_bbox(self.axes[0].bbox)
+            self.axes[i].axhline(y=self.plt_attr[i].alart_lim_u, xmin=0, xmax=1, color='red')
+            self.axes[i].axhline(y=self.plt_attr[i].alart_lim_u, xmin=0, xmax=1, color='red')
         
-        # update lines by prepared axes
-        # NOTE: lines become iterrable hereafter
-        self.lines = []
-        for i in range(self.__N_PLOTTER):
+            # update plot
+            # NOTE: lines become iterrable hereafter
             self.lines.append(self.axes[i].plot(self.x_series, self.y_series[i::self.__N_PLOTTER])[0])
      
-        for i in range(self.__N_PLOTTER):
             # reflect updates in lines
             self.axes[i].draw_artist(self.lines[i])
 
-        for i in range(self.__N_PLOTTER):
-            # redraw and show updated canvas
-            self.fig.canvas.blit(self.axes[i].bbox)
-        
+            # # redraw and show updated canvas
+            # self.fig.canvas.blit(self.axes[i].bbox)
+
+        # redraw and show updated canvas
+        self.fig.canvas.draw()
         # self.fig.canvas.flush_events()
         print("GUI PLT: redraw plots...")
 
     # Load configurations from external files
     def load_config_plotter(self):          
-        self.y_label = ["Series 1", "Series 2"]
-        self.y_min = [0.0, 0.0]
-        self.y_max = [4.0, 3.0]
-        self.alart_lim_u = [3.0, 2.5]
-        self.alart_lim_l = [1.0, 0.5]
+        ### TBREFAC.: TEMPORALLY DESIGNATED BY LITERALS ###
+        plt_attr_1 = PltAttr(
+            y_label =  "Series 1",
+            y_min = 0.0,
+            y_max = 4.0,
+            alart_lim_u = 3.0,
+            alart_lim_l = 1.0
+        )
+        plt_attr_2 = PltAttr(
+            y_label =  "Series 2",
+            y_min = 0.0,
+            y_max = 3.0,
+            alart_lim_u = 2.5,
+            alart_lim_l = 0.5,
+        )
+        plt_attr_3 = PltAttr(
+            y_label =  "Series 3",
+            y_min = 0.0,
+            y_max = 3.0,
+            alart_lim_u = 2.5,
+            alart_lim_l = 0.5,
+        )
+        plt_attr_4 = PltAttr(
+            y_label =  "Series 4",
+            y_min = 0.0,
+            y_max = 3.0,
+            alart_lim_u = 2.5,
+            alart_lim_l = 0.5,
+        )
+        plt_attr_5 = PltAttr(
+            y_label =  "Series 5",
+            y_min = 0.0,
+            y_max = 3.0,
+            alart_lim_u = 2.5,
+            alart_lim_l = 0.5,
+        )
+
+        self.plt_attr = []
+        self.plt_attr.append(plt_attr_1)
+        if self.__N_PLOTTER <= 1: return
+
+        self.plt_attr.append(plt_attr_2)
+        if self.__N_PLOTTER <= 2: return
+
+        self.plt_attr.append(plt_attr_3)
+        if self.__N_PLOTTER <= 3: return
+
+        self.plt_attr.append(plt_attr_4)
+        if self.__N_PLOTTER <= 4: return
+
+        self.plt_attr.append(plt_attr_5)
+        if self.__N_PLOTTER <= 5: return
+        else: self.__N_PLOTTER == 5
+
+        # self.y_label = [, "Series 2"]
+        # self.y_min = [0.0, 0.0]
+        # self.y_max = [4.0, 3.0]
+        # self.alart_lim_u = [3.0, 2.5]
+        # self.alart_lim_l = [1.0, 0.5]
         
         # self.item_plot = [self.df_cfg_plot['item'][self.df_cfg_plot['plot_1'].astype(bool)].iat[0],
         #                   self.df_cfg_plot['item'][self.df_cfg_plot['plot_2'].astype(bool)].iat[0]]
@@ -227,9 +278,9 @@ class MainPanel(wx.Panel):
         # print("GUI PLT: y_series = {}".format(self.y_series))
 
         # USED ONLY IN TEST
-        self.y_val = [y1_val, y2_val]   
+        self.y_val = [y1_val, y2_val, y3_val, y4_val, y5_val]   
 
-        # prepare empty matplotlib Fugure
+        # generate empty matplotlib Fugure
         self.fig = Figure(figsize=(8, 8))
         
         # register Figure with matplotlib Canvas
@@ -249,10 +300,10 @@ class MainPanel(wx.Panel):
             self.axes[i].set_xlim([t_min, t_min + self.__T_RANGE])
 
             # - set limit for y axis
-            self.axes[i].set_ylim([self.y_min[i], self.y_max[i]])
+            self.axes[i].set_ylim([self.plt_attr[i].y_min, self.plt_attr[i].y_max])
 
             # - set label for y axis
-            self.axes[i].set_ylabel(self.y_label[i])
+            self.axes[i].set_ylabel(self.plt_attr[i].y_label)
 
 
         # tentatively draw canvas without plot points to save as background
@@ -263,6 +314,20 @@ class MainPanel(wx.Panel):
         self.backgrounds = []
         for i in range(self.__N_PLOTTER):
             self.backgrounds.append(self.canvas.copy_from_bbox(self.axes[i].bbox))
+
+
+# retain plotter attributions
+class PltAttr():
+    def __init__(self, 
+                y_label="", y_unit="", 
+                y_min=0.0, y_max=1.0, 
+                alart_lim_l=0.0, alart_lim_u=1.0) -> None:
+        self.y_label = y_label
+        self.y_unit = y_unit
+        self.y_min = y_min
+        self.y_max = y_max
+        self.alart_lim_l = alart_lim_l
+        self.alart_lim_u = alart_lim_u
 
 
 if __name__ =="__main__":
