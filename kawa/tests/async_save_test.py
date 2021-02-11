c73e3134779e907edcd04b4bd229cf73efb76a26
kawasaki <kawasaki@nuae.nagoya-u.ac.jp>
2021-02-12 08:21:54 +0900

    update test

--- kawa/tests/async_save_test.py
+++ kawa/tests/async_save_test.py
@@ -7,22 +7,33 @@ import pandas as pd
 async def data_producer(queue):
     l = list(range(10))
 
-    for n in range(10):
+    # for n in range(10):
+    for n in range(1000):
         df = pd.DataFrame(np.array(l).reshape(-1, 5) + n)
         queue.put_nowait(df)
+        
+        await asyncio.sleep(0.0001)
     
     # print(f'queue = {queue}')
-    print(f'len = {queue.qsize()}')
+    # print(f'len = {queue.qsize()}')
 
     # await asyncio.sleep(1)
 
 async def data_consumer(queue):
     # await asyncio.sleep(1)
     
+    DATA_PATH = './data.csv'
+    df = pd.DataFrame()
+    df.to_csv(DATA_PATH, mode='w')
+
     while True:
-        item = await queue.get()
-        print()
-        print(f'item = {item}')
+        print(f'len = {queue.qsize()}')
+        
+        df = await queue.get()
+
+        df.to_csv(DATA_PATH, mode='a', header=False)
+        # print()
+        # print(f'item = {item}')
 
         queue.task_done()
 
@@ -30,9 +41,9 @@ async def main():
     queue = asyncio.Queue()
     
     tasks = []
-    task = asyncio.create_task(data_producer(queue))
-    tasks.append(task)
     task = asyncio.create_task(data_consumer(queue))
+    tasks.append(task)
+    task = asyncio.create_task(data_producer(queue))
     tasks.append(task)
 
     await asyncio.sleep(1)
