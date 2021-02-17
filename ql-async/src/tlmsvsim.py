f40e8baaa5405708e531abb2f4e350911d003b27
kawasaki <kawasaki@nuae.nagoya-u.ac.jp>
2021-02-17 14:12:25 +0900

    fix

--- ql-async/src/tlmsvsim.py
+++ ql-async/src/tlmsvsim.py
@@ -66,7 +66,7 @@ if __name__ == "__main__":
     if tlm_type == '':
         print("ERROR: TLM_TYPE is NOT designated!")
         sys.exit()
-    elif tlm_type != 'smt' and tlm_type != 'smt':
+    elif tlm_type != 'smt' and tlm_type != 'pcm':
         print("ERROR: TLM_TYPE is wrong!")
         sys.exit()
 
