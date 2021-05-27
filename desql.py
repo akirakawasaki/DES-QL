b0573e8df52f85fa817805e8135e6e23a4b3a5e3
kawasaki <kawasaki@nuae.nagoya-u.ac.jp>
2021-05-27 14:05:29 +0900

    v2.0.6 fixed

--- desql.py
+++ desql.py
@@ -25,8 +25,8 @@ def telemeter_handler_wrapper(tlm_type, q_msg, q_dgram):
     print(f'MAIN: Invoking UDP Communication Handler Wrapper for {tlm_type}...')
 
     # datagram server
-    HOST = '172.20.140.255'                                # mac
-    # HOST = socket.gethostbyname(socket.gethostname())      # windows / mac(debug)
+    # HOST = '172.20.140.255'                                # mac
+    HOST = socket.gethostbyname(socket.gethostname())      # windows / mac(debug)
     PORT =      60142 if (tlm_type == 'smt') \
            else 60140
 
